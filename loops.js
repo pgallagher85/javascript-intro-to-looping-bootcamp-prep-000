f31f8ca6dd51a58bc086f088e33c54f1d05f1da8
Patrick Gallagher <pgallagher85@gmail.com>
2017-11-13 17:23:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,9 @@
 function forLoop(array) {
-  for (i = 1; i < 25; i++) {
-  array.push("I am ${i} strange loop${i === 0 ? '' : 's'}"
+  for (let i = 0; i < 25; i++) {
+    if (i = 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push("I am ${i} strange loops.")
+    }
   }
 }
